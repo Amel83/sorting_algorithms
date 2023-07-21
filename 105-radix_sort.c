192f85c8e83d69d7caf25f74733510f5dc7a640c
amel83 <tesfaneshk22@gmail.com>
2023-07-21 16:02:03 -0700

    df

--- 105-radix_sort.c
+++ 105-radix_sort.c
@@ -10,16 +10,15 @@
  */
 int get_max(int *array, size_t size)
 {
-    size_t i;
-    int max = array[0];
+	size_t i;
+	int max = array[0];
 
-    for (i = 1; i < size; i++)
-    {
-        if (array[i] > max)
-            max = array[i];
-    }
-
-    return max;
+	for (i = 1; i < size; i++)
+	{
+		if (array[i] > max)
+			max = array[i];
+	}
+	return (max);
 }
 
 /**
@@ -30,29 +29,24 @@ int get_max(int *array, size_t size)
  */
 void counting_sort_radix(int *array, size_t size, int exp)
 {
-    int *output = malloc(size * sizeof(int));
-    int count[10] = {0};
-    size_t i;
+	int *output = malloc(size * sizeof(int));
+	int count[10] = {0};
+	size_t i;
 
-    if (output == NULL)
-        return;
-
-    for (i = 0; i < size; i++)
-        count[(array[i] / exp) % 10]++;
-
-    for (i = 1; i < 10; i++)
-        count[i] += count[i - 1];
-
-    for (i = size - 1; (int)i >= 0; i--)
-    {
-        output[count[(array[i] / exp) % 10] - 1] = array[i];
-        count[(array[i] / exp) % 10]--;
-    }
-
-    for (i = 0; i < size; i++)
-        array[i] = output[i];
-
-    free(output);
+	if (output == NULL)
+		return;
+	for (i = 0; i < size; i++)
+		count[(array[i] / exp) % 10]++;
+	for (i = 1; i < 10; i++)
+		count[i] += count[i - 1];
+	for (i = size - 1; (int)i >= 0; i--)
+	{
+		output[count[(array[i] / exp) % 10] - 1] = array[i];
+		count[(array[i] / exp) % 10]--;
+	}
+	for (i = 0; i < size; i++)
+		array[i] = output[i];
+	free(output);
 }
 
 /**
@@ -63,13 +57,12 @@ void counting_sort_radix(int *array, size_t size, int exp)
  */
 void radix_sort(int *array, size_t size)
 {
-    int max = get_max(array, size);
-    int exp;
+	int max = get_max(array, size);
+	int exp;
 
-    for (exp = 1; max / exp > 0; exp *= 10)
-    {
-        counting_sort_radix(array, size, exp);
-        print_array(array, size);
-    }
+	for (exp = 1; max / exp > 0; exp *= 10)
+	{
+		counting_sort_radix(array, size, exp);
+		print_array(array, size);
+	}
 }
-
