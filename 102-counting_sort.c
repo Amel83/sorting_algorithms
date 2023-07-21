965da120f5b470a111e4583adf50e0590fec0b12
amel83 <tesfaneshk22@gmail.com>
2023-07-21 15:50:38 -0700

    df

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -11,59 +11,41 @@
 void counting_sort(int *array, size_t size)
 {
 	int i, max = 0;
-    int *count_array = NULL, *output = NULL;
-    if (array == NULL || size <= 1)
-        return;
+	int *count_array = NULL, *output = NULL;
 
-
-    for (i = 0; i < (int)size; i++)
-    {
-        if (array[i] > max)
-            max = array[i];
-    }
-
-
-    count_array = malloc((max + 1) * sizeof(int));
-    if (count_array == NULL)
-        return;
-
-    for (i = 0; i <= max; i++)
-        count_array[i] = 0;
-
-
-    for (i = 0; i < (int)size; i++)
-        count_array[array[i]]++;
-
-
-    for (i = 1; i <= max; i++)
-        count_array[i] += count_array[i - 1];
-
-
-    output = malloc(size * sizeof(int));
-    if (output == NULL)
-    {
-        free(count_array);
-        return;
-    }
-
-    for (i = size - 1; i >= 0; i--)
-    {
-        output[count_array[array[i]] - 1] = array[i];
-        count_array[array[i]]--;
-    }
-
-
-    for (i = 0; i < (int)size; i++)
-        array[i] = output[i];
-
-
-    printf("%d", count_array[0]);
-    for (i = 1; i <= max; i++)
-        printf(", %d", count_array[i]);
-    printf("\n");
-
-
-    free(count_array);
-    free(output);
+	if (array == NULL || size <= 1)
+		return;
+	for (i = 0; i < (int)size; i++)
+	{
+		if (array[i] > max)
+			max = array[i];
+	}
+	count_array = malloc((max + 1) * sizeof(int));
+	if (count_array == NULL)
+		return;
+	for (i = 0; i <= max; i++)
+		count_array[i] = 0;
+	for (i = 0; i < (int)size; i++)
+		count_array[array[i]]++;
+	for (i = 1; i <= max; i++)
+		count_array[i] += count_array[i - 1];
+	output = malloc(size * sizeof(int));
+	if (output == NULL)
+	{
+		free(count_array);
+		return;
+	}
+	for (i = size - 1; i >= 0; i--)
+	{
+		output[count_array[array[i]] - 1] = array[i];
+		count_array[array[i]]--;
+	}
+	for (i = 0; i < (int)size; i++)
+		array[i] = output[i];
+	printf("%d", count_array[0]);
+	for (i = 1; i <= max; i++)
+		printf(", %d", count_array[i]);
+	printf("\n");
+	free(count_array);
+	free(output);
 }
-
--- 103-merge_sort.c
+++ 103-merge_sort.c
@@ -12,76 +12,64 @@
  */
 void merge(int *array, int *left, int *right, size_t size_left, size_t size_right)
 {
-    size_t i = 0, j = 0, k = 0;
-    int *tmp = malloc((size_left + size_right) * sizeof(int));
+	size_t i = 0, j = 0, k = 0;
+	int *tmp = malloc((size_left + size_right) * sizeof(int));
 
-    if (tmp == NULL)
-        return;
-
-    printf("Merging...\n[left]: ");
-    print_array(left, size_left);
-    printf("[right]: ");
-    print_array(right, size_right);
-
-    while (i < size_left && j < size_right)
-    {
-        if (left[i] <= right[j])
-        {
-            tmp[k] = left[i];
-            i++;
-        }
-        else
-        {
-            tmp[k] = right[j];
-            j++;
-        }
-        k++;
-    }
-
-    while (i < size_left)
-    {
-        tmp[k] = left[i];
-        i++;
-        k++;
-    }
-
-    while (j < size_right)
-    {
-        tmp[k] = right[j];
-        j++;
-        k++;
-    }
-
-    for (i = 0; i < size_left + size_right; i++)
-        array[i] = tmp[i];
-
-    printf("[Done]: ");
-    print_array(array, size_left + size_right);
-
-    free(tmp);
+	if (tmp == NULL)
+		return;
+	printf("Merging...\n[left]: ");
+	print_array(left, size_left);
+	printf("[right]: ");
+	print_array(right, size_right);
+	while (i < size_left && j < size_right)
+	{
+		if (left[i] <= right[j])
+		{
+			tmp[k] = left[i];
+			i++;
+		}
+		else
+		{
+			tmp[k] = right[j];
+			j++;
+		}
+		k++;
+	}
+	while (i < size_left)
+	{
+		tmp[k] = left[i];
+		i++;
+		k++;
+	}
+	while (j < size_right)
+	{
+		tmp[k] = right[j];
+		j++;
+		k++;
+	}
+	for (i = 0; i < size_left + size_right; i++)
+		array[i] = tmp[i];
+	printf("[Done]: ");
+	print_array(array, size_left + size_right);
+	free(tmp);
 }
 
 /**
- * merge_sort - Sorts an array of integers in ascending order using the
- *              Merge Sort algorithm (Top-down approach).
+ * merge_sort - Merge Sort algorithm.
  * @array: Pointer to the array of integers.
  * @size: Size of the array.
  */
 void merge_sort(int *array, size_t size)
 {
-    
+	size_t middle = size / 2;
+	ize_t size_left = middle;
+	size_t size_right = size - middle;
+	int *left = array;
+	int *right = array + middle;
 
-    size_t middle = size / 2;
-    size_t size_left = middle;
-    size_t size_right = size - middle;
-    int *left = array;
-    int *right = array + middle;
-
-    if (array == NULL || size <= 1)
-        return;
-    merge_sort(left, size_left);
-    merge_sort(right, size_right);
-
-    merge(array, left, right, size_left, size_right);
+	if (array == NULL || size <= 1)
+		return;
+	merge_sort(left, size_left);
+	merge_sort(right, size_right);
+	merge(array, left, right, size_left, size_right);
 }
-
