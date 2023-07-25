ddff2510895100ac5b1e01dbb1201ed5ebad9513
amel83 <tesfaneshk222@gmail.com>
2023-07-25 14:05:13 -0700

    j

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -24,7 +24,7 @@ void bubble_sort(int *array, size_t size)
 				temp = array[j];
 				array[j] = array[j + 1];
 				array[j + 1] = temp;
-				s = 1;
+				flag = 1;
 				print_array(array, size);
 			}
 		}
